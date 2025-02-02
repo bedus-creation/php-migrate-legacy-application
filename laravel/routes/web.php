be6864ed5c6e6b44e8ff42088d45a5750142370a
Bedram Tamang <tmgbedu@gmail.com>
2025-02-01 23:43:09 -0800

    fix: change route to use `~` for new application

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', function () {
+    return [
+        "posts" => [
+            "id" => 1
+        ]
+    ];
+});
